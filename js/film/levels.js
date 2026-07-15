249983ee35e29d605bd7c70c1ec09790cda2cdce
dickv940-star <dickv940@gmail.com>
2026-07-15 12:27:31 +0700

    Update levels.js

--- js/film/levels.js
+++ js/film/levels.js
@@ -9,4 +9,87 @@ AppDIGI
 
 const Levels = {
 
-    // Persent
+
+    /*
+    Auto contrast / levels adjustment
+    */
+
+    apply(imageData){
+
+
+        if(!imageData) return null;
+
+
+        const data = imageData.data;
+
+
+        let min = 255;
+        let max = 0;
+
+
+        // Cari range pixel
+
+        for(let i = 0; i < data.length; i += 4){
+
+
+            let r = data[i];
+            let g = data[i+1];
+            let b = data[i+2];
+
+
+            let avg = (r + g + b) / 3;
+
+
+            if(avg < min) min = avg;
+
+            if(avg > max) max = avg;
+
+
+        }
+
+
+
+        let range = max - min;
+
+
+        if(range === 0) return imageData;
+
+
+
+        // Apply levels
+
+        for(let i = 0; i < data.length; i += 4){
+
+
+            data[i] =
+                ((data[i] - min) / range) * 255;
+
+
+            data[i+1] =
+                ((data[i+1] - min) / range) * 255;
+
+
+            data[i+2] =
+                ((data[i+2] - min) / range) * 255;
+
+
+        }
+
+
+        return imageData;
+
+
+    }
+
+
+
+};
+
+
+
+window.Levels = Levels;
+
+
+console.log(
+    "Levels Engine Loaded"
+);
